51896c69b0be46abc4fbb6d94289b5f227812341
2323223231 <s22194@heungdeok.ms.kr>
2025-01-18 14:49:12 +0900

    Create  week7.cpp

---
+++ week7.cpp
@@ -0,0 +1,101 @@
+// ConsoleApplication2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//40-1
+/*
+int main()
+{
+	int a;
+	scanf("%d", &a);
+
+	if (a < 0) {
+		printf("0보다 자금");
+	}
+	else if (a > 0) {
+		printf("0ㅂㄷㅋ");
+	}
+}
+*/
+//40-2
+
+/*
+int main() {
+	int a;
+	scanf("%d", &a);
+
+	if (a % 2 == 1)
+	{
+		printf("홀수");
+
+	}
+	else
+
+	{
+		printf("짝수");
+	}
+
+	
+}
+*/
+
+//40 - 3
+
+/*
+int main()
+{
+	int a;
+	scanf("%d", &a);
+
+	if(a % 3 == 0)
+	{
+		printf("3의 배수임");
+
+	}
+	else
+
+	{
+		printf("ㄴㄴㄴ");
+	}
+
+
+}
+*/
+
+/*
+int main() {
+	float  a1, a2, a3, a4, a5;
+
+
+	printf("5ㄱ");
+	scanf("%f %f %f %f %f", &a1, &a2, &a3, &a4, &a5);
+
+	
+	float  b = a1;
+
+	if (a2 > b) b = a2;
+	if (a3 > b) b = a3;
+	if (a4 > b) b = a4;
+	if (a5 > b) b = a5;
+
+	printf("%f", b);
+
+}
+*/
+
+int main()
+{
+int a;
+scanf("%d", &a);
+
+if (a == 2)
+{
+	printf("Cervelo t4");
+
+}
+
+}
+
+
